3f00d72a64090dc926b9d9ee6d25217748e6e244
Carlos Valcarcel <40213289+losDaniel@users.noreply.github.com>
2020-03-24 16:51:08 -0500

    Update spot_utils.py

--- spot_connect/spot_connect/spot_utils.py
+++ spot_connect/spot_connect/spot_utils.py
@@ -24,11 +24,14 @@ def absoluteFilePaths(directory):
         for f in filenames:
             yield os.path.abspath(os.path.join(dirpath, f))
 
+def pull_root(): 
+    '''Retrieve the directory for this instance'''
+    return Path(os.path.dirname(os.path.abspath(__file__)))
 
 def load_profiles():
     '''Load the profiles from the package profile.txt file'''
     
-    profile = [f for f in list(absoluteFilePaths(pull_root().parent)) if f.split('\\')[-1]=='profiles.txt'][0]    
+    profile = [f for f in list(absoluteFilePaths(pull_root())) if f.split('\\')[-1]=='profiles.txt'][0]    
     
     with open(profile,'r') as f:
         profiles = ast.literal_eval(f.read())
@@ -37,60 +40,38 @@ def load_profiles():
         
     return profiles
 
-
 def show_instances(): 
     client = boto3.client('ec2', region_name='us-west-2')
     print('Instances (by Key names):')
     for i in [res['Instances'][0] for res in client.describe_instances()['Reservations']]:
         print('     - "'+i['KeyName'].split('-')[1]+'" Type: '+i['InstanceType']+', ID: '+i['InstanceId'], flush=True)
 
-
 def printTotals(transferred, toBeTransferred):
     '''Print paramiko upload transfer'''
     print("Transferred: %.3f" % float(float(transferred)/float(toBeTransferred)), end="\r", flush=True)
 
-
 def get_package_kp_dir():
     '''Get the key-pair directory'''
-    return pull_root().parent+'\\kp_dir\\'
-
+    kpfile = [f for f in list(absoluteFilePaths(pull_root())) if f.split('\\')[-1]=='profiles.txt'][0]    
+    with open(kpfile,'r') as f: 
+        default_path = f.read()
+        f.close()
+    return default_path 
 
 def set_default_kp_dir(directory : str): 
     '''Set the default key pair directory'''
-
-    # Get the package key pair path 
-    kp_dir = get_package_kp_dir()                         
-
-    # Write the defulat path to a text file in the package folder
-    with open(kp_dir+'\\KP_default_path.txt', 'w') as f:   
-        default_path = f.write(directory)            
-        f.close()    
+    kpfile = [f for f in list(absoluteFilePaths(pull_root())) if f.split('\\')[-1]=='profiles.txt'][0]    
+    with open(kpfile,'w') as f: 
+        f.write(directory)
+        f.close()
     print('Default path has been set to '+default_path)    
 
-
 def get_default_kp_dir(): 
-    '''Get the default key pair directory'''
-    
-    # Get the package key pair path 
-    kp_dir = get_package_kp_dir()                         
-
-    # If an alternative default path has been set then we can load there 
-    if os.path.exists(kp_dir):        
-        with open(kp_dir+'\\KP_default_path.txt', 'r') as f:
-            default_path = f.read()
-            f.close() 
-    
-    # Otherwise the package key-pair directory will work
-    else: 
-        default_path = kp_dir
-    
-    return default_path
-
-
-def pull_root(): 
-    '''Retrieve the directory for this instance'''
-    return Path(os.path.dirname(os.path.abspath(__file__)))
-
-    
+    '''Get the default key pair directory'''    
+    kp_dir = get_package_kp_dir()                             
+    if kp_dir =='': 
+        raise Exception('Please use the "set_default_kp_dir" method to set a default key-pair storage directory. You only need to do this once.')
+    return kp_dir
+ 
 if __name__ == '__main__':
     pull_root()
